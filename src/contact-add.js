464a5ed49f2c7591b4ac682af3ed4c235fb423bf
Shivdhwaj Pandey <shivdhwaj@browntape.com>
2016-09-08 12:18:16 +0530

    contact add page updated

--- src/contact-add.js
+++ src/contact-add.js
@@ -1,31 +1,53 @@
     
-import {EventAggregator} from 'aurelia-event-aggregator';
+// import {EventAggregator} from 'aurelia-event-aggregator';
+// import {WebAPI} from './web-api';
+// //import {ContactUpdated, ContactViewed} from './messages';
+
+// export class ContactAdd {
+// //  static inject = [WebAPI, EventAggregator];
+
+//   //constructor(api, ea){
+//     // this.api = api;
+//     // this.contacts = [];
+
+//     // ea.subscribe(ContactViewed, msg => this.select(msg.contact));
+//     // ea.subscribe(ContactUpdated, msg => {
+//     //   let id = msg.contact.id;
+//     //   let found = this.contacts.find(x => x.id === id);
+//     //   Object.assign(found, msg.contact);
+//     // });
+//   //}
+
+//   // created(){
+//   //   this.api.getContactList().then(contacts => this.contacts = contacts);
+//   // }
+
+//   // select(contact){
+//   //   this.selectedId = contact.id;
+//   //   return true;
+//   // }
+// }
+
+//   
+
+    
 import {WebAPI} from './web-api';
-//import {ContactUpdated, ContactViewed} from './messages';
 
-export class ContactAdd {
-//  static inject = [WebAPI, EventAggregator];
+export class ContactList {
+  static inject() { return [WebAPI] };
 
-  //constructor(api, ea){
-    // this.api = api;
-    // this.contacts = [];
+  constructor(api){
+    this.api = api;
+    this.contacts = [];
+  }
 
-    // ea.subscribe(ContactViewed, msg => this.select(msg.contact));
-    // ea.subscribe(ContactUpdated, msg => {
-    //   let id = msg.contact.id;
-    //   let found = this.contacts.find(x => x.id === id);
-    //   Object.assign(found, msg.contact);
-    // });
-  //}
+  created(){
+    //this.api.getContactList().then(contacts => this.contacts = contacts);
+  }
 
-  // created(){
-  //   this.api.getContactList().then(contacts => this.contacts = contacts);
-  // }
-
-  // select(contact){
-  //   this.selectedId = contact.id;
-  //   return true;
-  // }
+  select(contact){
+    this.selectedId = contact.id;
+    return true;
+  }
 }
 
-  
