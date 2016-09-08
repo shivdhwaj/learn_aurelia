ca5eb5181cc605ae06bf287fa80497cf308211a9
Shivdhwaj Pandey <shivdhwaj@browntape.com>
2016-09-08 12:24:23 +0530

    Added add contact rout

--- src/app.js
+++ src/app.js
@@ -12,7 +12,8 @@ export class App {
     config.title = 'Contacts';
     config.map([
       { route: '',              moduleId: 'no-selection',   title: 'Select'},
-      { route: 'contacts/:id',  moduleId: 'contact-detail', name:'contacts' }
+      { route: 'contacts/:id',  moduleId: 'contact-detail', name:'contacts' },
+      { route: 'contacts/:add',  moduleId: 'contact-add',title: 'Add Contact', name:'add contacts' }
     ]);
 
     this.router = router;
